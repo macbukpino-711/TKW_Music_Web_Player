0464c0ffdb98d389ffd58a19d78905c219fd8115
Your Name <you@example.com>
2026-03-29 23:49:43 +0700

    fix song.js in menu.js

--- menu.js
+++ menu.js
@@ -30,7 +30,7 @@ const fallbackSongs = window.fallbackSongs ?? [];
 
 async function loadSongs() {
   try {
-    const response = await fetch("../json/song.json");
+    const response = await fetch("song.json");
     if (!response.ok) {
       throw new Error("Could not load song.json");
     }
